d8d279b788eaf503e54714ff7133b3fe58ac2683
sakshi778718 <singhswati167an@gmail.com>
2026-06-23 18:41:19 +0530

    Implement user registration and login endpoints

---
+++ users.py
@@ -0,0 +1,77 @@
+from fastapi import APIRouter,Depends,HTTPException
+from sqlalchemy.orm import Session
+
+from ..database import get_db
+from ..models import User
+from ..schemas import UserCreate,UserLogin
+from ..auth import (
+    hash_password,
+    verify_password,
+    create_access_token
+)
+
+router = APIRouter()
+
+@router.post("/register")
+def register(
+    user:UserCreate,
+    db:Session = Depends(get_db)
+):
+
+    existing = db.query(User).filter(
+        User.username == user.username
+    ).first()
+
+    if existing:
+        raise HTTPException(
+            status_code=400,
+            detail="Username already exists"
+        )
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=hash_password(
+            user.password
+        )
+    )
+
+    db.add(new_user)
+    db.commit()
+
+    return {
+        "message":"User Registered"
+    }
+
+@router.post("/login")
+def login(
+    user:UserLogin,
+    db:Session=Depends(get_db)
+):
+
+    db_user = db.query(User).filter(
+        User.username == user.username
+    ).first()
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    if not verify_password(
+        user.password,
+        db_user.password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    token = create_access_token(
+        {"sub":db_user.username}
+    )
+
+    return {
+        "access_token":token
+    }
